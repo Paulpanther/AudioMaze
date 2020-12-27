9b7a146c86d04fa93504fc248bbfefafc40006ae
Paul M <paul.methfessel@web.de>
2020-12-27 15:20:48 +0100

    Project init

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float runSpeed = 20f;
+	public float moveLimiter = 0.7f;
+
+	private Rigidbody2D _body;
+	private float _horizontal, _vertical;
+
+	private void Start() {
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		_horizontal = Input.GetAxisRaw("Horizontal");
+		_vertical = Input.GetAxisRaw("Vertical");
+	}
+
+	private void FixedUpdate() {
+		if (Mathf.Abs(_horizontal + _vertical) > 1.1f) {
+			_horizontal *= moveLimiter;
+			_vertical *= moveLimiter;
+		}
+
+		_body.velocity = new Vector2(_horizontal * runSpeed, _vertical * runSpeed);
+	}
+}
