cb09b960dd1a207b8f1008ccf83f3da5d5f35005
Leon Bein <leon.bein@student.hpi.de>
2020-12-31 12:41:00 +0100

    Multiple audio sources for multiple directions #9

--- Assets/Scripts/DistanceNoiser.cs
+++ Assets/Scripts/DistanceNoiser.cs
@@ -12,14 +12,17 @@ using UnityEngine;
 
 public class DistanceNoiser : MonoBehaviour
 {
+
+    public int numDirections = 4;
+
     //For mixing purposes, multiply volume with volume constant 
     public float volumeConstant = 1;
+    //Audio Clip for Wall Noise
+    public AudioClip wallSound;
     //Audio Source for wall Noise
-    public AudioSource wallNoise;
+    private AudioSource[] wallNoise;
     //distance where sound begins to play
     public float triggerDistance;
-
-    private float currentPitch = 1.0f;
 
     //Direction Vectors
     private static Vector3 up = new Vector3(0, 1, 0);
@@ -28,40 +31,31 @@ public class DistanceNoiser : MonoBehaviour
     private static Vector3 right = new Vector3(-1, 0, 0);
     private Vector3[] directions = new Vector3[]{up,down,left,right };
 
+	private void Start()
+	{
+		wallNoise = new AudioSource[numDirections];
+        for(int i = 0; i < numDirections; i++) {
+            float associatedPitch = 1.4f - 0.2f * (float)i;
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = wallSound;
+            audioSource.pitch = associatedPitch;
+            wallNoise[i] = audioSource;
+        }
+	}
 
     void playSound(int direction, float distance)
     {
-        wallNoise.volume = 1 - distance*volumeConstant;
-        float associatedPitch = 0;
-        switch (direction)
+        AudioSource noiseOfDirection = wallNoise[direction];
+        if (distance <= triggerDistance)
+        {            
+            noiseOfDirection.volume = 1 - distance*volumeConstant;
+            if(!noiseOfDirection.isPlaying) noiseOfDirection.Play();
+        } 
+        else 
         {
-            case 0:
-                //Debug.Log("playing from above " + distance);
-                associatedPitch = 1.4f;
-                break;
-            case 1:
-                //Debug.Log("playing from below " + distance);
-                associatedPitch = 1.2f;
-                break;
-            case 2:
-                //Debug.Log("playing from the right " + distance);
-                associatedPitch = 1.0f;
-                break;
-            case 3:
-                //Debug.Log("playing from the left " + distance);
-                associatedPitch = 0.8f;
-                break;
+            if(noiseOfDirection.isPlaying) noiseOfDirection.Stop();
         }
-        if (distance <= triggerDistance && !wallNoise.isPlaying)
-        {
-            
-            wallNoise.Play();
-        }
-        if(currentPitch != associatedPitch)
-        {
-            currentPitch = associatedPitch;
-            wallNoise.pitch = associatedPitch;
-        }
+
     }
 
     // Update is called once per frame
@@ -73,28 +67,13 @@ public class DistanceNoiser : MonoBehaviour
         RaycastHit hitright = new RaycastHit();
 
         RaycastHit[] hits = new RaycastHit[] {hitup, hitdown, hitleft, hitright};
-        int hitnum = 0;
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < numDirections; i++)
         {
-            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y,0),directions[i]* triggerDistance, Color.red);
             //Debug.Log(i + " casting ray in direction" + directions[i] + " from "+ gameObject.transform.position);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, directions[i],triggerDistance, 1 << LayerMask.NameToLayer("Walls"));
-            if (hit)
-            {
-                //Debug.Log(hit.collider.gameObject);
-
-                if (hit.collider.gameObject.tag == "Wall")
-                {
-                    hitnum++;
-                    Debug.Log(i + " hits");
-                    playSound(i, hit.distance);
-                }
-            }
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, directions[i], 100, 1 << LayerMask.NameToLayer("Walls"));
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y,0), directions[i].normalized * triggerDistance, hit.distance <= triggerDistance ? Color.red : Color.green);
+            playSound(i, hit.distance);
         
-        }
-        if (hitnum == 0)
-        {
-            wallNoise.Stop();
         }
 
     }
