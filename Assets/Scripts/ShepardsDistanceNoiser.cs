4b20009dddd344610cd1575a1375e29de5ad5c5f
Leon Bein <leon.bein@student.hpi.de>
2021-01-23 21:08:18 +0100

    Add Shepards Distance Noiser

---
+++ Assets/Scripts/ShepardsDistanceNoiser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShepardsDistanceNoiser : ShepardsScale
+{
+
+    public float conversionFactor = 0.02f;
+
+    void Update()
+    {
+        Player player = GetComponentInParent<Player>();
+        this.shift = player.distanceChange * conversionFactor;
+    }
+}
