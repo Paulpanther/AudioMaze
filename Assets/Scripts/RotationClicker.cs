800ab9b01fe65bb830828f416911eb5dbbf214e4
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-02-01 18:00:34 +0100

    WIP: Rotation clicks

---
+++ Assets/Scripts/RotationClicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RotationClicker : MonoBehaviour
+{
+    public int majorTickCount = 4;
+    public int minorTickCount = 12;
+
+    private float prevTime = 0; 
+    void FixedUpdate()
+    {
+        // 0 = 360 is north, 90 is west, 180 is south, 270 is east
+        var degree = transform.rotation.eulerAngles.z;
+        if (Time.fixedTime - prevTime > 0.5)
+        {
+            Debug.Log(GetNearestTick(degree, majorTickCount));
+            Debug.Log(GetNearestTick(degree, minorTickCount));
+            prevTime = Time.fixedTime;
+        }
+    }
+
+    private float GetNearestTick(float degree, int tickCount)
+    {
+        return degree / (360f / tickCount);
+    }
+}
