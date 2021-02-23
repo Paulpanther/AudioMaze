077d57a06c21ff11d42a56d0bd3e9b7a396a9830
Paul Methfessel <paul.methfessel@web.de>
2021-02-23 15:57:11 +0100

    Add ui mixer for draught and master

---
+++ Assets/Scripts/ToggleUIMixerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleUIMixerScript : MonoBehaviour
+{
+
+    private bool _visible = false;
+    
+    private void Start()
+    {
+        UpdateChildren();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            _visible = !_visible;
+            UpdateChildren();
+        }
+    }
+
+    private void UpdateChildren()
+    {
+        if (_visible)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/UIMixerSlider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class UIMixerSlider : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public string parameterName;
+
+    private void Start()
+    {
+        mixer.GetFloat(parameterName, out var volume);
+        GetComponent<Slider>().value = Mathf.Pow(10, volume / 20);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(parameterName, Mathf.Log10(sliderValue) * 20);
+    }
+}
